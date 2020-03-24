1f2cc4dbbf30ddebe8fc449bb6be4e9f398bdfff
Benjamin D. Peterson <petersonben50@gmail.com>
2020-03-24 15:13:15 -0500

    Added scripts to clean metagenomes and assemble them by group with metaspades

---
+++ code/assembly_processing/assembly_processing.md
@@ -0,0 +1,71 @@
+### Assembly Processing for 2019 intensive
+
+This document outlines the protocol I followed for processing the metagenomes and assemblies from the Everglades.
+It will also include my notes about the process, and directions to other files as needed.
+
+**Data retrieval and inspection**
+
+First thing I did was retrieve the data.
+The first round of sequencing was through Michigan State's sequencing center.
+I was able to retrieve it using `wget`.
+All these metagenomes are in `~/Everglades/dataRaw/metagenomes`.
+
+I next made a list with all the metagenomes of interest: `metadata/lists/metagenome_list.csv`.
+This list will be used to check and trim the metagenomes.
+
+
+**Trimming metagenomes**
+
+Next I quality trimmed the metagenome.
+To do this, I used the fastp program (version 0.20.0), which trims the metagenome to our liking and gives us the stats about the metagenome both before and after trimming.
+I download the program using conda, into our bioinformatics virtual environment.
+
+I used this program to save out both the paired and unpaired reads after trimming.
+I also stored the failed reads, just so we can check to see what's driving the loss of reads if need be.
+
+I cut the tail end of the reads based on the read quality.
+The front end quality of the reads are usually pretty good so I didn't bother with trimming the front or using the `cut_right option`.
+I used a 10bp window with a quality score cutoff of 20.
+Since I was trimming these down, I wanted to cut out any short reads that get overtrimmed.
+Length filtering is set to 15 by default, but I changed it to 100.
+I left the default quality filtering in place as well, which is a quality limit of 15, and a maximum of 40% of bases in a read below that threshold.
+I also merged the reads.
+For some reason, once I use the merge function, the single reads are filled in at the end, so don't get worried if you're checking the size of the files as the program runs and the single reads file is empty.
+I then went through and took a look at the output of fastp, the files of which are stored here: `/Users/benjaminpeterson/Documents/research/Everglades/dataRaw/metagenomes/2019_intensive/fastp`.
+
+-
+
+
+
+
+Unfortunately fastp doesn't read out how many single reads vs. paired end reads there are, so I ended up running fastQC on the data as well just to get those counts and get some other information on the read quality.
+I'll also save the read counts here: `~/Everglades/dataEdited/metagenomes/2019_intensive_ancillary_info/metagenome_read_count.tsv`, then download it to `dataEdited/metagenomes/reports/metagenome_read_count.tsv`.
+
+
+
+
+
+
+**Metagenome assembly**
+
+I ran individual assemblies as well as a co-assembly for these metagenomes.
+All using both metaSPADes and MegaHit, and will evaluate them afterwards to see which ones were the best.
+To set this up, I made a spreadsheet for each assembly program to use (`metadata/lists/assembly_groups_metaspades.csv` and `metadata/lists/assembly_groups_megahit.csv`).
+This spreadsheet has two columns, "assemblyID" and "metagenomeID".
+Each metagenome that is to be a part of an assembly gets it's own row:
+So, if coassembly_2 is going to use ENP18_1 and ENP18_2, this would is what the sheet would have:
+
+assemblyID,metagenomeID
+coassembly_2,ENP18_1
+coassembly_2,ENP18_2
+
+I didn't do the surface water samples from 2018 for now, since in our initial analysis they weren't of much interest.
+
+Also, before starting assemblies, I needed to upload the two `assembly_by_group` python scripts for MegaHit and metaSPADes, to `code/assembly_processing`.
+
+*Assemble the metagenome*
+
+Once the metagenomes were trimmed and ready to go, I assembled them using metaSPADes (v3.13.2).
+For now, I focused on getting individual assemblies.
+I included the paired reads, the merged reads, and the single reads.
+For kmers, I included 21, 33, 55, 77, 99, and 127.
---
+++ code/assembly_processing/assembly_processing.sh
@@ -0,0 +1,227 @@
+#!/bin/sh
+
+######################
+# code/assembly_processing.sh
+# Benjamin D. Peterson
+
+# This set of scripts will contain everything
+# needed for the initial processing of the
+# metagenomes.
+######################
+
+
+############################################
+############################################
+# Data retrieval and inspection
+############################################
+############################################
+
+######################
+# KMBP004: First round of metagenomes
+######################
+
+# Get set up
+mkdir ~/Everglades/dataRaw/metagenomes
+
+
+# Now retrieve the metagenomes using lftp
+# (in the lftp virtual environment)
+# Log onto GLBRC server
+#screen -S Everglades_MG_transfer
+#cd Everglades/dataRaw/metagenomes
+
+#/opt/wget-1.19/bin/wget -r -np -nH --ask-password ftps://mcmahonk@titan.bch.msu.edu/20180921_DNASeq_PE150
+#mv 20180921_DNASeq_PE150/* ./
+#rm -fr 20180921_DNASeq_PE150
+#exit
+
+
+# Let's get a list of our metagenomes.
+# Save out a list of all the metagenome IDs here:
+
+
+
+############################################
+############################################
+# Trimming metagenomes
+############################################
+############################################
+
+screen -S Everglades_metagenome_trimming
+mkdir ~/Everglades/dataEdited
+mkdir ~/Everglades/dataEdited/metagenomes
+mkdir ~/Everglades/dataEdited/metagenomes/reports
+
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PERL5LIB=""
+
+
+
+######################
+# Trim metagenomes
+######################
+
+cd ~/Everglades/dataRaw/metagenomes
+
+read_storage=~/Everglades/dataEdited/metagenomes
+ancillary_info=~/Everglades/dataEdited/metagenomes/reports
+
+cat ~/Everglades/metadata/lists/metagenome_list.csv | while read metagenome
+do
+
+  if [ ! -e $read_storage/$metagenome\_R1.fastq.gz ]; then
+
+    echo "Processing" $metagenome
+
+    fastp --in1 $metagenome*R1*fastq.gz \
+          --in2 $metagenome*R2*fastq.gz \
+          --out1 $read_storage/$metagenome\_R1.fastq.gz \
+          --out2 $read_storage/$metagenome\_R2.fastq.gz \
+          --unpaired1 $read_storage/$metagenome\_single.fastq.gz \
+          --unpaired2 $read_storage/$metagenome\_single.fastq.gz \
+          --merge \
+          --merged_out $read_storage/$metagenome\_merged.fastq.gz \
+          --failed_out $ancillary_info/$metagenome\_failed.fastq.gz \
+          --html $ancillary_info/$metagenome\_report.html \
+          --detect_adapter_for_pe \
+          --cut_tail \
+          --cut_tail_window_size 10 \
+          --cut_tail_mean_quality 20 \
+          --length_required 100
+
+
+    fastqc -o $ancillary_info \
+            $read_storage/$metagenome\_R1.fastq.gz
+    fastqc -o $ancillary_info \
+            $read_storage/$metagenome\_R2.fastq.gz
+    fastqc -o $ancillary_info \
+            $read_storage/$metagenome\_single.fastq.gz
+
+  else
+
+    echo "Already processed" $metagenome
+
+  fi
+
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+######################
+# Count reads in metagenome
+######################
+
+screen -S Everglades_metagenome_read_counting
+
+cd ~/Everglades/dataRaw/metagenomes
+
+read_storage=~/Everglades/dataEdited/metagenomes
+ancillary_info=~/Everglades/dataEdited/metagenomes/reports
+
+echo -e "metagenomeID\tforwardReads\treverseReads\tsingleReads\tmergedReads" > $ancillary_info/metagenome_read_count.tsv
+
+cat ~/Everglades/metadata/lists/metagenome_list.csv | while read metagenome
+do
+
+  echo "Working on" $metagenome
+
+  forwardCount=$(zgrep -c "^@" $read_storage/$metagenome\_R1.fastq.gz)
+  reverseCount=$(zgrep -c "^@" $read_storage/$metagenome\_R2.fastq.gz)
+  singleCount=$(zgrep -c "^@" $read_storage/$metagenome\_single.fastq.gz)
+  mergeCount=$(zgrep -c "^@" $read_storage/$metagenome\_merged.fastq.gz)
+
+  echo -e $metagenome"\t"$forwardCount"\t"$reverseCount"\t"$singleCount"\t"$mergeCount >> $ancillary_info/metagenome_read_count.tsv
+
+done
+
+
+
+
+
+
+
+
+############################################
+############################################
+# Metagenome assembly
+############################################
+############################################
+
+mkdir ~/Everglades/code/assembly_processing
+
+######################
+# Assemblies by metaSPades
+######################
+
+screen -S Everglades_metagenome_coassembly
+
+cd ~/Everglades/dataEdited/assemblies
+mkdir ~/Everglades/dataEdited/assemblies
+mkdir ~/Everglades/dataEdited/assemblies/assembly_files/
+
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=""
+PERL5LIB=""
+
+code=~/Everglades/code/assembly_processing/
+assembly_grouping=~/Everglades/metadata/lists/assembly_groups_metaspades.csv
+read_storage=~/Everglades/dataEdited/metagenomes/
+output=~/Everglades/dataEdited/assemblies/assembly_files/
+
+
+# Make sure the list of wanted assemblies by metaSPADes is up to date
+# in ~/Everglades/dataEdited/assemblies/assembly_group_metaspades.csv
+
+# Get a list of group names
+cd ~/Everglades/metadata/lists/
+tail -n +2 assembly_groups_metaspades.csv | \
+    awk -F ',' '{ print $1 }' | \
+    uniq \
+    > assembly_list_metaspades.txt
+
+cd ~/Everglades/dataEdited/assemblies
+
+cat ~/Everglades/metadata/lists/assembly_list_metaspades.txt | while read assembly
+do
+
+  if [ ! -d $output/$assembly ]; then
+    mkdir $output/$assembly
+  fi
+
+  # Run group assemblies with metaSPADes
+
+  if [ ! -e $output/$assembly/scaffolds.fasta ]; then
+    echo "Assembling" $assembly
+    python $code/assembly_by_group_metaspades.py $assembly \
+                                                  $assembly_grouping \
+                                                  $read_storage \
+                                                  $output/$assembly
+    # To continue a paused run:
+    # metaspades.py --continue -o $assembly
+
+  else
+    echo $assembly "already assembled"
+  fi
+
+done
+
+
+#exit
