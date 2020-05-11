125fd3feb52b62c7041a51db9c80496868868091
Benjamin D. Peterson <petersonben50@gmail.com>
2020-05-11 12:02:18 -0500

    Updated scripts to clean hgcA abundance

--- code/2018_analysis_assembly/hgcA/clean_hgcA_abundance.R
+++ code/2018_analysis_assembly/hgcA/clean_hgcA_abundance.R
@@ -18,6 +18,11 @@ normalized.coverage.vector <- readRDS("dataEdited/metagenomes/reports/metagenome
 
 
 
+# Read in list of good hgcA seqs
+hgcA.list <- readLines("dataEdited/2018_analysis_assembly/hgcA/hgcA.txt")
+
+
+
 #### List of file names with depth info ####
 
 list.o.depth.files <- list.files(path = "dataEdited/2018_analysis_assembly/hgcA/depth",
@@ -28,7 +33,7 @@ list.o.depth.files <- list.files(path = "dataEdited/2018_analysis_assembly/hgcA/
 
 
 #### Read in and normalize depth data ####
-raw.depth.counts <- lapply(list.o.depths,
+raw.depth.counts <- lapply(list.o.depth.files,
                            function(fileName) {
                              
                              metagenomeID = fileName %>%
@@ -46,17 +51,55 @@ raw.depth.counts <- lapply(list.o.depths,
 
 
 
+
 #### Combine data into dataframe ####
 
-mean.df <- do.call(rbind,
-                   raw.depth.counts) %>%
-  spread(key = read.origin, value = depth, fill = NA)
+depth.data <- do.call(rbind,
+                      raw.depth.counts) %>%
+  spread(key = read.origin, value = depth, fill = NA) %>%
+  rename(scaffoldID = seqID)
 
 
 
 
-#### Write out file
 
-write.csv(mean.df,
+
+#### Write out file with depths for all sequences ####
+
+write.csv(depth.data,
           "dataEdited/2018_analysis_assembly/hgcA/depth/hgcA_coverage.csv",
           row.names = FALSE)
+
+
+
+
+
+
+#### Clean depth file for final hgcA seqs ####
+
+# Get a scaffold to ORF vector
+scaffold.to.ORF <- sapply(depth.data$scaffoldID,
+                          function(x) {
+                            grep(x, hgcA.list, value = TRUE)
+                          }) %>%
+  unlist()
+
+# Keep only depth info for good seqs and sum abundance
+depth.data.final <- depth.data %>%
+  mutate(seqID = scaffold.to.ORF[scaffoldID]) %>%
+  filter(!is.na(seqID)) %>%
+  mutate(total = ENP18_001_002_003 + ENP18_024_025 +
+           ENP18_030_032 + ENP18_048_049_50 + ENP18_061) %>%
+  select(seqID, scaffoldID, length, total,
+         ENP18_001_002_003, ENP18_024_025,
+           ENP18_030_032, ENP18_048_049_50, ENP18_061)
+
+
+
+
+
+#### Read out depth file for final hgcA seqs ####
+write.csv(depth.data.final,
+          "dataEdited/2018_analysis_assembly/hgcA/depth/hgcA_coverage_final.csv",
+          row.names = FALSE)
+
