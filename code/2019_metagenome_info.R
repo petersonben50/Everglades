b73eacf9dc7638fdf68091f927c571e12211aff7
Benjamin D. Peterson <petersonben50@gmail.com>
2021-02-01 00:43:45 -0600

    Script to gather metagenome info

---
+++ code/2019_metagenome_info.R
@@ -0,0 +1,67 @@
+#### code/2019_metagenome_info.R ####
+# Benjamin D. Peterson
+
+
+#### Clean up ####
+rm(list = ls())
+setwd("~/Documents/research/Everglades/")
+library(lubridate)
+library(readxl)
+library(tidyverse)
+
+
+#### Read in data ####
+
+pre.trim.reads <- read.table("dataEdited/metagenomes/reports/metagenome_read_count_pre_trimming.tsv",
+                             stringsAsFactors = FALSE,
+                             header = TRUE) %>%
+  rename(paired_reads_pre_trimming = forwardReads) %>%
+  select(-reverseReads)
+
+post.trim.reads <- read.table("dataEdited/metagenomes/reports/metagenome_read_count.tsv",
+                              stringsAsFactors = FALSE,
+                              header = TRUE) %>%
+  rename(paired_reads = forwardReads) %>%
+  select(-reverseReads)
+
+unique.mapped.reads <- read.table("dataEdited/2019_analysis_assembly/reports/uniq_mapped_reads_MG.tsv",
+                                  stringsAsFactors = FALSE)
+names(unique.mapped.reads) <- c("metagenomeID", "mappedReads_forward",
+                                "mappedReads_reverse", "mappedReads_single")
+
+
+coverage <- read.table("dataEdited/metagenomes/reports/metagenome_coverage.tsv",
+                       stringsAsFactors = FALSE,
+                       header = TRUE) %>%
+  mutate(total_coverage = R1, R2, single, merged) %>%
+  select(metagenomeID, total_coverage)
+
+MG.metadata <- read_xlsx("metadata/metagenomes/metagenome_metadata.xlsx")
+
+
+
+#### Calculate fraction of reads mapped ####
+fraction.mapped.reads <- left_join(unique.mapped.reads,
+                                   post.trim.reads) %>%
+  mutate(total.mapped = mappedReads_forward + mappedReads_reverse + mappedReads_single,
+         total.reads = (paired_reads * 2) + singleReads + mergedReads,
+         fraction.mapped = total.mapped / total.reads * 100) %>%
+  select(metagenomeID, fraction.mapped)
+
+
+
+#### Combine data ####
+metagenome.data <- MG.metadata %>%
+  left_join(pre.trim.reads) %>%
+  left_join(post.trim.reads) %>%
+  left_join(fraction.mapped.reads) %>%
+  left_join(coverage) %>%
+  filter(year(ymd(date)) == 2019) %>%
+  as.data.frame()
+
+
+
+#### Write out data table ####
+write.csv(x = metagenome.data,
+          "results/2019_metagenome_info.csv",
+          row.names = FALSE)
