c37e28d2cc657d2606ddbb7854de66b0840adcf3
Benjamin D. Peterson <petersonben50@gmail.com>
2021-02-13 13:05:56 -0600

    Changed hgcA tree cleaning script to use tree with Hg-MATE

--- code/2019_analysis_assembly/hgcA/clean_hgcA_tree.R
+++ code/2019_analysis_assembly/hgcA/clean_hgcA_tree.R
@@ -10,6 +10,7 @@ rm(list = ls())
 setwd("/Users/benjaminpeterson/Documents/research/Everglades/")
 library(ggtree)
 library(phangorn)
+library(readxl)
 library(tidyverse)
 library(treeio)
 
@@ -32,49 +33,6 @@ hgcA.references.confirmed.vector <- readRDS("/Users/benjaminpeterson/Box/referen
 ref.confirmed.colors <- readRDS("/Users/benjaminpeterson/Box/references/hgcA/hgcA_confirmed_ref_color_vector.rds")
 
 
-
-
-
-#### Read in reference data for McDaniel 2020 ####
-hgcA.references.McD <- read.csv("/Users/benjaminpeterson/Box/references/hgcA/McDaniel_2020/McDaniel_2020_hgcA_ref_metadata.csv",
-                                stringsAsFactors = FALSE) %>%
-  select(genome_name, name) %>%
-  mutate(tree_name = paste(strsplit(name, " ") %>%
-                             sapply("[", 1),
-                           " ",
-                           strsplit(name, " ") %>%
-                             sapply("[", 2),
-                           " (",
-                           genome_name,
-                           ")",
-                           sep = ""))
-hgcA.references.McD.vector <- hgcA.references.McD$tree_name
-names(hgcA.references.McD.vector) <- hgcA.references.McD$genome_name
-
-
-#### Read in reference data for nr database ####
-hgcA.references.nr <- read.table("references/hgcA/2019_assembly_analysis/hgcA_ref_nr_taxonomy.tsv",
-                                 stringsAsFactors = FALSE,
-                                 sep = '\t') %>%
-  rename(accessionID = V1,
-         name = V2) %>%
-  mutate(tree_name = paste(strsplit(name, " ") %>%
-                             sapply("[", 1),
-                           " ",
-                           strsplit(name, " ") %>%
-                             sapply("[", 2),
-                           " (",
-                           accessionID,
-                           ")",
-                           sep = "")) %>%
-  select(accessionID, tree_name)
-
-hgcA.references.nr.vector <- hgcA.references.nr$tree_name
-names(hgcA.references.nr.vector) <- hgcA.references.nr$accessionID
-
-
-
-
 #### Check out FastTree of final alignment ####
 
 # Read in tree
@@ -88,68 +46,125 @@ hgcA.tree <- midpoint(hgcA.tree.unrooted,
                       node.labels = "support")
 
 # Get indices
-reference.indices.McD <- which(hgcA.tree$tip.label %in% names(hgcA.references.McD.vector))
-reference.indices.nr <- which(hgcA.tree$tip.label %in% names(hgcA.references.nr.vector))
 reference.indices.confirmed <- which(hgcA.tree$tip.label %in% names(hgcA.references.confirmed.vector))
 this.study.indices <- which(hgcA.tree$tip.label %in% hgcA.list)
 
 # Change names:
-hgcA.tree$tip.label[reference.indices.McD] <- hgcA.references.McD.vector[hgcA.tree$tip.label[reference.indices.McD]]
-hgcA.tree$tip.label[reference.indices.nr] <- hgcA.references.nr.vector[hgcA.tree$tip.label[reference.indices.nr]]
 hgcA.tree$tip.label[reference.indices.confirmed] <- hgcA.references.confirmed.vector[hgcA.tree$tip.label[reference.indices.confirmed]]
 
 # Set color vector
 color.vector <- rep("grey", length(hgcA.tree$tip.label))
-color.vector[c(reference.indices.McD, reference.indices.nr)] <- colorblind.color.vector["black"]
 color.vector[reference.indices.confirmed] <- colorblind.color.vector[ref.confirmed.colors[hgcA.tree$tip.label[reference.indices.confirmed]]]
 color.vector[this.study.indices] <- "red"
 
 # Visualize tree
 pdf("results/2019_analysis_assembly/hgcA_tree_FastTree.pdf",
-    height = 30,
+    height = 120,
     width = 8)
-ggtree(hgcA.tree, aes(x = 0,
-                      xend = 5)) + 
-  geom_tiplab(size=2.5, align = TRUE, col = color.vector) + 
-  geom_nodelab(aes(x = branch),
-               vjust = -.3,
-               size = 1)
+ggtree(hgcA.tree) + 
+  geom_tiplab(size=2.5, align = TRUE, col = color.vector) +
+  geom_text2(aes(subset=!isTip, label=node),
+             size = 1.5)
 dev.off()
+
+
+
+#### Pull out sequence names to remove ####
+
+nodes.to.remove <- c(1671, 1766, 1599, 1619, 1626,
+                     1628, 1472, 1415, 1388, 1376,
+                     1537, 1372, 1545, 1157, 1267,
+                     1286, 1137, 1295, 1845, 1829,
+                     1953, 1984, 1999, 1941, 1929,
+                     1919, 1906, 1897, 2052, 2045,
+                     2039, 2030, 2083, 2087, 2097,
+                     2090, 2102, 2112, 2124, 2126,
+                     2128)
+seqs.to.remove <- vector()
+for (node.of.interest in nodes.to.remove) {
+  tree.subset.to.remove <- tree_subset(hgcA.tree,
+                                       node = node.of.interest,
+                                       levels_back = 0)
+  seqs.to.remove <- c(seqs.to.remove, tree.subset.to.remove$tip.label)
+}
+seqs.to.remove <- grep("_ISO_",
+                       seqs.to.remove,
+                       invert = TRUE,
+                       value = TRUE)
+
+fileConn <- file("dataEdited/2019_analysis_assembly/hgcA/phylogeny/seqs_to_remove.txt")
+writeLines(seqs.to.remove, fileConn)
+close(fileConn)
+
+# Clean up
+rm(list = ls())
+
 
 
 
 #### Clean up RAxML tree from final alignment ####
 
+
+#### Read in needed files ####
+
+hgcA.list <- readLines("dataEdited/2019_analysis_assembly/hgcA/hgcA.txt")
+colorblind.color.vector <- readRDS("~/Box/ancillary_science_stuff/colors/colorblind_friendly_colors_R/colorblind_friendly_colors.rds")
+hgcA.references.confirmed.vector <- readRDS("/Users/benjaminpeterson/Box/references/hgcA/hgcA_confirmed_ref_renaming_vector.rds")
+ref.confirmed.colors <- readRDS("/Users/benjaminpeterson/Box/references/hgcA/hgcA_confirmed_ref_color_vector.rds")
+
+#### Read in Hg-MATE seq info ####
+hg.mate.metadata <- read_xlsx("/Users/benjaminpeterson/Documents/research/Hg_MATE/versions/v1.01142021/Hg-MATE-Db.v1.01142021_catalogue.xlsx") %>%
+  rename(group = `microbial group`,
+         name = `[Organism Name]_Phylum-Class`) %>%
+  mutate(name = gsub("sp._", "sp.", name)) %>%
+  mutate(name = paste(name %>% strsplit("_") %>% sapply("[", 1),
+                      name %>% strsplit("_") %>% sapply("[", 2),
+                      sep = "_"),
+         treeID = paste(group,
+                        " (", name, "-", MATE_id, ")",
+                        sep = ""))
+MATE.renaming.vector <- hg.mate.metadata$treeID
+names(MATE.renaming.vector) <- hg.mate.metadata$MATE_id
+           
+
 # Read in tree
 tree.name <- "dataEdited/2019_analysis_assembly/hgcA/phylogeny/RAxML_bipartitions.hgcA"
 hgcA.tree.unrooted <- read.newick(tree.name)
 rm(tree.name)
-
+#### Fix tip labels ####
+hgcA.tree.unrooted$tip.label <- paste(hgcA.tree.unrooted$tip.label %>% strsplit("_") %>% sapply("[", 1),
+                                      hgcA.tree.unrooted$tip.label %>% strsplit("_") %>% sapply("[", 2),
+                                      hgcA.tree.unrooted$tip.label %>% strsplit("_") %>% sapply("[", 3),
+                                      sep = "_")
 
 # Root tree
-hgcA.tree <- midpoint(hgcA.tree.unrooted,
-                      node.labels = "support")
+hgcA.tree <- root(phy = hgcA.tree.unrooted,
+                  outgroup = c("paralog_Thermosulfurimonas_dismutans",
+                               "paralog_Candidatus_Omnitrophica",
+                               "Sed993Meta19_000000003033_5",
+                               "Sed993Meta19_000000001064_12",
+                               "Sed993Meta19_000000027668_3",
+                               "Sed993Mega19_000001097352_5",
+                               "Sed993Meta19_000000035661_4"),
+                  edgelabel = TRUE)
+
 
 # Get indices
-reference.indices.McD <- which(hgcA.tree$tip.label %in% names(hgcA.references.McD.vector))
-reference.indices.nr <- which(hgcA.tree$tip.label %in% names(hgcA.references.nr.vector))
-reference.indices.confirmed <- which(hgcA.tree$tip.label %in% names(hgcA.references.confirmed.vector))
+reference.indices.mate <- which(hgcA.tree$tip.label %in% names(MATE.renaming.vector))
 this.study.indices <- which(hgcA.tree$tip.label %in% hgcA.list)
 
 # Change names:
-hgcA.tree$tip.label[reference.indices.McD] <- hgcA.references.McD.vector[hgcA.tree$tip.label[reference.indices.McD]]
-hgcA.tree$tip.label[reference.indices.nr] <- hgcA.references.nr.vector[hgcA.tree$tip.label[reference.indices.nr]]
-hgcA.tree$tip.label[reference.indices.confirmed] <- hgcA.references.confirmed.vector[hgcA.tree$tip.label[reference.indices.confirmed]]
+hgcA.tree$tip.label[reference.indices.mate] <- MATE.renaming.vector[hgcA.tree$tip.label[reference.indices.mate]]
+
 
 # Set color vector
 color.vector <- rep("grey", length(hgcA.tree$tip.label))
-color.vector[c(reference.indices.McD, reference.indices.nr)] <- colorblind.color.vector["black"]
-color.vector[reference.indices.confirmed] <- colorblind.color.vector[ref.confirmed.colors[hgcA.tree$tip.label[reference.indices.confirmed]]]
-color.vector[this.study.indices] <- "red"
+color.vector[reference.indices.mate] <- colorblind.color.vector["black"]
+color.vector[this.study.indices] <- colorblind.color.vector["vermillion"]
 
 # Visualize tree
-pdf("results/2019_analysis_assembly/hgcA_tree_RAxML_midpointRooting.pdf",
-    height = 30,
+pdf("results/2019_analysis_assembly/hgcA_tree_RAxML_rooted.pdf",
+    height = 60,
     width = 10)
 ggtree(hgcA.tree, aes(x = 0,
                       xend = 4.5)) + 
@@ -158,6 +173,8 @@ ggtree(hgcA.tree, aes(x = 0,
                vjust = -.3,
                size = 2)
 dev.off()
+
+
 
 
 #### Save out tree ####
