48d74a1fbc9c3c2eedb2f3136a03d25e7cd3f711
Benjamin D. Peterson <petersonben50@gmail.com>
2021-09-28 13:41:52 -0500

    Mapping stats

--- code/assembly_analysis/mapping/mapping_stats.R
+++ code/assembly_analysis/mapping/mapping_stats.R
@@ -1,4 +1,4 @@
-#### code/2019_analysis_assembly/mapping_stats.R ####
+#### code/assembly_analysis/mapping/mapping_stats.R ####
 # Benjamin D. Peterson
 # Analysis in 2019_analysis_assembly/mapping_assembly.md
 
@@ -12,7 +12,7 @@ library(tidyverse)
 #### Read in data ####
 
 # Read in overall mapping of each metagenome
-uniq.mapped.reads <- read.table("dataEdited/2019_analysis_assembly/reports/uniq_mapped_reads_MG.tsv",
+uniq.mapped.reads <- read.table("dataEdited/assembly_analysis/reports/uniq_mapped_reads_MG.tsv",
                                 sep = "\t",
                                 col.names = c("metagenomeID", "forwardReads",
                                               "reverseReads", "singleReads"))
@@ -27,10 +27,17 @@ all.reads <- read.table("dataEdited/metagenomes/reports/metagenome_read_count.ts
 # Read in metadata
 MG.metadata <- read_xlsx("metadata/metagenomes/metagenome_metadata.xlsx")
 
-#### Combine data and calculate fraction mapping to all  ####
 
+#### Combine data and calculate fraction mapping to all  ####
 all.data <- MG.metadata %>%
   right_join(uniq.mapped.reads) %>%
   left_join(all.reads) %>%
   mutate(percentMappingAllMGs = (forwardReads + reverseReads + singleReads) / totalReads * 100) %>%
   select(-sampleID)
+
+
+
+#### Save out data ####
+write.csv(all.data,
+          "results/metagenomes/housekeeping/mapping_stats.csv",
+          row.names = FALSE)
