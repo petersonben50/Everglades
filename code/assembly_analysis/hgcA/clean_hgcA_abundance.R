57d45b4f4b6cff157571f5fdf1ec47f7b370022a
Benjamin D. Peterson <petersonben50@gmail.com>
2021-09-28 17:17:00 -0500

    Updated cleaning script

--- code/assembly_analysis/hgcA/clean_hgcA_abundance.R
+++ code/assembly_analysis/hgcA/clean_hgcA_abundance.R
@@ -1,36 +1,28 @@
-#### code/2019_analysis_assembly/clean_hgcA_coverage.R ####
+#### code/assembly_analysis/hgcA/clean_hgcA_coverage.R ####
 # Written by Benjamin D. Peterson
 
+# hgcA abundance normalized to the SCG coverage
 
-#### First: Normalize to the read count ####
 
 #### Get ready. Get set ####
-
 rm(list = ls())
 setwd("/Users/benjaminpeterson/Documents/research/Everglades/")
 library(stringr)
 library(tidyverse)
 
 
-
 #### Read in normalization vector ####
-
-normalized.coverage.vector <- readRDS("dataEdited/metagenomes/reports/metagenome_normalization_vector.rds")
-
+normalized.coverage.vector <- readRDS("dataEdited/assembly_analysis/SCGs/scg_normalization_vector.rds")
 
 
-# Read in list of good hgcA seqs
-hgcA.list <- readLines("dataEdited/2019_analysis_assembly/hgcA/identification/hgcA_good.txt")
-
+#### Read in list of good hgcA seqs ####
+hgcA.list <- readLines("dataEdited/assembly_analysis/hgcA/identification/hgcA_good.txt")
 
 
 #### List of file names with depth info ####
-
-list.o.depth.files <- list.files(path = "dataEdited/2019_analysis_assembly/hgcA/depth",
+list.o.depth.files <- list.files(path = "dataEdited/assembly_analysis/hgcA/depth",
                                  pattern = "hgcA_depth",
                                  full.names = TRUE)
-
-
 
 
 #### Read in and normalize depth data ####
@@ -38,129 +30,7 @@ raw.depth.counts <- lapply(list.o.depth.files,
                            function(fileName) {
                              
                              metagenomeID = fileName %>%
-                               gsub("dataEdited/2019_analysis_assembly/hgcA/depth/", "", .) %>%
-                               gsub("_hgcA_depth.tsv", "", .)
-                             
-                             read.table(fileName,
-                                        stringsAsFactors = FALSE,
-                                        col.names = c("seqID", "depth", "length")) %>%
-                               mutate(depth = depth * normalized.coverage.vector[metagenomeID]) %>%
-                               mutate(read.origin = metagenomeID)
-                             
-                             })
-
-
-
-
-
-#### Combine data into dataframe ####
-
-depth.data <- do.call(rbind,
-                      raw.depth.counts) %>%
-  spread(key = read.origin, value = depth, fill = NA) %>%
-  rename(scaffoldID = seqID)
-
-
-
-
-
-
-#### Write out file with depths for all sequences ####
-
-# write.csv(depth.data,
-#           "dataEdited/2019_analysis_assembly/hgcA/depth/hgcA_coverage_all.csv",
-#           row.names = FALSE)
-
-
-
-
-
-
-#### Clean depth file for final hgcA seqs ####
-
-# Get a scaffold to ORF vector
-scaffold.to.ORF <- sapply(depth.data$scaffoldID,
-                          function(x) {
-                            grep(x, hgcA.list, value = TRUE)
-                          }) %>%
-  unlist()
-
-# Keep only depth info for good seqs and sum abundance
-depth.data.final <- depth.data %>%
-  mutate(seqID = scaffold.to.ORF[scaffoldID]) %>%
-  filter(!is.na(seqID)) %>%
-  mutate(total = KMBP005A + KMBP005B + KMBP005C + KMBP005D + KMBP005E +
-           KMBP005F + KMBP005G + KMBP005H + KMBP005I + KMBP005J + KMBP005K +
-           KMBP005L + KMBP006A + KMBP006B + KMBP006C + KMBP006D + KMBP006E +
-           KMBP006F) %>%
-  select(seqID, scaffoldID, length, total, KMBP005A, KMBP005B,
-         KMBP005C, KMBP005D, KMBP005E, KMBP005F, KMBP005G,
-         KMBP005H, KMBP005I, KMBP005J, KMBP005K, KMBP005L,
-         KMBP006A, KMBP006B, KMBP006C, KMBP006D, KMBP006E, 
-         KMBP006F)
-
-
-
-
-
-#### Read out depth file for final hgcA seqs ####
-# write.csv(depth.data.final,
-#           "dataEdited/2019_analysis_assembly/hgcA/depth/hgcA_coverage.csv",
-#           row.names = FALSE)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#### Second: Normalize to the SCG coverage ####
-
-#### Get ready. Get set ####
-
-rm(list = ls())
-setwd("/Users/benjaminpeterson/Documents/research/Everglades/")
-library(stringr)
-library(tidyverse)
-
-
-
-#### Read in normalization vector ####
-
-normalized.coverage.vector <- readRDS("dataEdited/2019_analysis_assembly/SCGs/scg_normalization_vector.rds")
-
-
-
-# Read in list of good hgcA seqs
-hgcA.list <- readLines("dataEdited/2019_analysis_assembly/hgcA/identification/hgcA_good.txt")
-
-
-
-#### List of file names with depth info ####
-
-list.o.depth.files <- list.files(path = "dataEdited/2019_analysis_assembly/hgcA/depth",
-                                 pattern = "hgcA_depth",
-                                 full.names = TRUE)
-
-
-
-
-#### Read in and normalize depth data ####
-raw.depth.counts <- lapply(list.o.depth.files,
-                           function(fileName) {
-                             
-                             metagenomeID = fileName %>%
-                               gsub("dataEdited/2019_analysis_assembly/hgcA/depth/", "", .) %>%
+                               gsub("dataEdited/assembly_analysis/hgcA/depth/", "", .) %>%
                                gsub("_hgcA_depth.tsv", "", .)
                              
                              read.table(fileName,
@@ -172,41 +42,26 @@ raw.depth.counts <- lapply(list.o.depth.files,
                            })
 
 
-
-
-
 #### Combine data into dataframe ####
-
 depth.data <- do.call(rbind,
                       raw.depth.counts) %>%
   spread(key = read.origin, value = depth, fill = NA) %>%
   rename(scaffoldID = seqID)
 
 
-
-
-
-
 #### Write out file with depths for all sequences ####
-
 write.csv(depth.data,
-          "dataEdited/2019_analysis_assembly/hgcA/depth/hgcA_coverage_raw_scgNormalization.csv",
+          "dataEdited/assembly_analysis/hgcA/depth/hgcA_coverage_raw_scgNormalization.csv",
           row.names = FALSE)
 
 
-
-
-
-
 #### Clean depth file for final hgcA seqs ####
-
 # Get a scaffold to ORF vector
 scaffold.to.ORF <- sapply(depth.data$scaffoldID,
                           function(x) {
                             grep(x, hgcA.list, value = TRUE)
                           }) %>%
   unlist()
-
 # Keep only depth info for good seqs and sum abundance
 depth.data.final <- depth.data %>%
   mutate(seqID = scaffold.to.ORF[scaffoldID]) %>%
@@ -222,11 +77,7 @@ depth.data.final <- depth.data %>%
          KMBP006F)
 
 
-
-
-
 #### Read out depth file for final hgcA seqs ####
 write.csv(depth.data.final,
-          "dataEdited/2019_analysis_assembly/hgcA/depth/hgcA_coverage_scgNormalization.csv",
+          "dataEdited/assembly_analysis/hgcA/depth/hgcA_coverage_scgNormalization.csv",
           row.names = FALSE)
-
